429a25d69fb64e28e97d1d2e8f86e6be27cd18e0
Aline Montório <montorioline@gmail.com>
2025-09-19 16:41:00 -0300

    Novas Modificações

---
+++ src/WebApi/Repositories/GenericRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Context;
+using WebApi.Repositories;
+
+public class GenericRepository<T> : IGenericRepository<T> where T: class
+{
+    protected readonly AppDbContext _context;
+    protected readonly DbSet<T> _dbSet;
+    public GenericRepository(AppDbContext context)
+    {
+        _context = context;
+        _dbSet = _context.Set<T>();
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync(int pageNumber, int pageSize)
+    {
+        return await _dbSet.Skip((pageNumber - 1)* pageSize).Take(pageSize).ToListAsync();
+    }
+    public async Task<T?> GetByIdAsync(Guid id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+    public async Task AddAsync(T entity)
+    {
+        await _dbSet.AddAsync(entity);
+    }
+    public void Update(T entity)
+    {
+        _dbSet.Update(entity);
+    }
+    public void Remove(T entity)
+    {
+        _dbSet.Remove(entity);
+    }
+}
