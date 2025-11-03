a6ef3c90e6a8bec0ff88aafd44ad6e8fb7f0ef7c
Aline Montório <montorioline@gmail.com>
2025-11-03 14:24:59 -0300

    feat: add authentication to Swagger

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -20,6 +20,7 @@ using WebApi.Security;
 using WebApi.Services.Implementations;
 using WebApi.Services.Interfaces;
 using System.Reflection;
+using Microsoft.OpenApi.Models;
 
 // TODO: quebrar esse arquivo em classes menores dependencyInjection por camada
 // e metodos por separacao: registerRepositories, registerServices...
@@ -33,11 +34,36 @@ if (builder.Environment.IsDevelopment() && string.IsNullOrEmpty(builder.Configur
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen (c => 
+builder.Services.AddSwaggerGen(c => 
 {
     var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
     var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
     c.IncludeXmlComments(xmlPath);
+
+    // Configuração de segurança JWT
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
 });
 
 // authentication
